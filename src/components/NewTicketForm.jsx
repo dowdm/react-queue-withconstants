7c9f993ec20e2574d9201a731bf08d6338cccce6
William Chu <williamchu51@gmail.com>
2018-06-30 13:45:29 -0700

    add form ref variables

--- src/components/NewTicketForm.jsx
+++ src/components/NewTicketForm.jsx
@@ -1,20 +1,37 @@
 import React from 'react';
 
 function NewTicketForm(){
+  let names = null;
+  let location = null;
+  let issue = null;
+
+  function handleNewTicketFormSubmission(event) {
+    event.preventDefault();
+    console.log(names.value);
+    console.log(location.value);
+    console.log(issue.value);
+    names.value = '';
+    location.value = '';
+    issue.value = '';
+  }
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleNewTicketFormSubmission}>
         <input
           type='text'
           id='names'
-          placeholder='Pair Names'/>
+          placeholder='Pair Names'
+          ref={(input) => {names = input;}} />
         <input
           type='text'
           id='location'
-          placeholder='Location'/>
+          placeholder='Location'
+          ref={(input) => {location = input;}} />
         <textarea
           id='issue'
-          placeholder='Describe your issue.'/>
+          placeholder='Describe your issue.'
+          ref={(input) => {issue = input;}} />
         <button type='submit'>Help!</button>
       </form>
     </div>
